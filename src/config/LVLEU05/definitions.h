39e17642db241498e177dc035ca4c68c811600ae
Michele Barbato <michele.barbato@email.com>
2024-02-09 22:12:08 +0100

    Minima configurazione trovata

--- src/config/LVLEU05/definitions.h
+++ src/config/LVLEU05/definitions.h
@@ -50,22 +50,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/sercom/i2c_master/plib_sercom2_i2c_master.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/eic/plib_eic.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
